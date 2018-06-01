45a483b319f804d2dbf8579db4095c57012cb237
Sang Kim <ssos3182@hotmail.com>
2018-06-01 15:07:32 -0400

    Added basic SplashActivity and some basic animations

---
+++ app/src/main/java/com/sangbk3/hearthstonedb/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.sangbk3.hearthstonedb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 1000);
+    }
+}
